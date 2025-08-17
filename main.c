0d56731f0b562178e126bb229022d0ccb0b78cc4
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2025-08-17 14:18:34 +0200

    busy wait all 3 ADC channels, set GPIOs according to threshold and power down for 5s via TC1

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
- /*
+/*
  * MAIN Generated Driver File
  * 
  * @file main.c
@@ -10,7 +10,7 @@
  * @version MAIN Driver Version 1.0.2
  *
  * @version Package Version: 3.1.2
-*/
+ */
 
 /*
 © [2025] Microchip Technology Inc. and its subsidiaries.
@@ -31,21 +31,95 @@
     TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
     EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
     THIS SOFTWARE.
-*/
+ */
 #include "mcc_generated_files/system/system.h"
+#include "mcc_generated_files/power/power.h"
+#include "mcc_generated_files/system/utils/utils_assert.h"
 
 /*
     Main application
-*/
+ */
 
-int main(void)
-{
+#define NUMBER_PLANTS (3)
+#define DRY_THRESHOLD (500)
+
+void set_gpio(int i) {
+    switch (i) {
+        case 0:
+        {
+            IO_PD2_SetHigh();
+            break;
+        }
+        case 1:
+        {
+            IO_PD3_SetHigh();
+            break;
+        }
+        case 2:
+        {
+            IO_PD4_SetHigh();
+            break;
+        }
+        default:
+        {
+            ASSERT(false);
+            break;
+        }
+    }
+}
+
+void clear_gpio(int i) {
+    switch (i) {
+        case 0:
+        {
+            IO_PD2_SetLow();
+            break;
+        }
+        case 1:
+        {
+            IO_PD3_SetLow();
+            break;
+        }
+        case 2:
+        {
+            IO_PD4_SetLow();
+            break;
+        }
+        default:
+        {
+            ASSERT(false);
+            break;
+        }
+    }
+}
+
+int main(void) {
     SYSTEM_Initialize();
-    
-    ADC_Initialize();
+    ASSERT(!POWER_PeripheralEnable(POWER_TIM1));
 
-    while(1)
-    {
-        
-    }    
+    while (1) {
+        ASSERT(!POWER_PeripheralEnable(POWER_ADC));
+        ADC_Enable();
+
+        for (int i = 0; i < NUMBER_PLANTS; ++i) {
+            ADC_ChannelSelect(ADC0 + i);
+            ADC_ConversionStart();
+            while (!ADC_IsConversionDone()) {
+                __asm("nop;");
+            }
+            const volatile adc_result_t adc = ADC_ConversionResultGet();
+            const uint16_t mask = (1 << ADC_ResolutionGet()) - 1;
+            const uint16_t humidity = adc & mask;
+
+            if (humidity > DRY_THRESHOLD) {
+                set_gpio(i);
+            } else {
+                clear_gpio(i);
+            }
+        }
+
+        ADC_Disable();
+        ASSERT(!POWER_PeripheralDisable(POWER_ADC));
+        ASSERT(!POWER_LowPowerModeEnter(POWER_IDLE_MODE));
+    }
 }
